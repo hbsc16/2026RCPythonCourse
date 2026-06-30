873e273b3036ff6e6b07994ce4ab8126a95d1ec3
hannah@Firebird <hbsc16@gmail.com>
2026-06-30 13:22:29 -0700

    Day 1 python labs

---
+++ test.py
@@ -0,0 +1,8 @@
+age = 36
+favorite_number = 16
+print(f"I am {age} years old, and my favorite number is {favorite_number}.")
+hobby = "singing"
+hrsperweek = "2.5"
+print("""My name is Hannah.
+^_00_^
+ ^_00_^""")
