298c13ae7ed4fd20bc3f7e271447421b42ceb770
hannah@Firebird <hbsc16@gmail.com>
2026-07-01 09:25:11 -0700

    Test mystery treasure game

---
+++ Treasure.py
@@ -0,0 +1,9 @@
+selection = input("Please choose a treasure box: a, b, or c:\n")
+if (selection == "a"):
+    print("You just won a day pass to Washington State Parks!")
+elif (selection == "b"):
+    print("You just won two tickets to a Taylor Swift concert!")
+elif (selection == "c"):
+    print("You just won a pass to the sunflower festival!")
+else:
+    print("Sorry, I didn't understand that.")
