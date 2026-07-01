16b8b20d478a0869a8c6dd2be6bae4d8fea3580d
hannah@Firebird <hbsc16@gmail.com>
2026-07-01 14:42:30 -0700

    Day 3

---
+++ game_stats_tracker.py
@@ -0,0 +1,12 @@
+player_name = "Amir"
+level = 5
+score = 120
+bonus = 30
+final_score = score + bonus
+
+print("Player Report")
+print("Name: " + player_name)
+print("Level: " + str(level))
+print("Score: " + str(score))
+print("Bonus: " + str(bonus))
+print("Final Score: " + str(final_score))
---
+++ test2.py
@@ -0,0 +1,8 @@
+code1 = "200"
+code2 = "40"
+
+sum = int(code1) + int(code2)
+print(sum)
+
+code3 = sum * 2
+print("Your final code is " + )
